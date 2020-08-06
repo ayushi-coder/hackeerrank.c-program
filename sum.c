fd50277ac7e74dcccbfad47275fcc8f0c68d7a51
ayushi-coder <65589755+ayushi-coder@users.noreply.github.com>
2020-08-06 08:40:09 -0600

    sum

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+
+    int n1, n2, sum;
+    printf("enter two integers");
+    scanf("%d%d", &n1,&n2);
+
+    sum = (n1+n2);
+
+    printf("%d",sum);
+    return 0;
+
+}
